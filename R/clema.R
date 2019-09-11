312cce15c7eb33deebd9d16d83099b45489aa46f
Roberto Salas <wala.rsalas@gmail.com>
2019-09-11 17:06:57 -0300

    added replace=T

--- R/clema.R
+++ R/clema.R
@@ -2,13 +2,21 @@
 #'
 #' @param words A word or a vector of words to lemmatize
 #'
-#' @return A converted vector of words
+#' @return A lemmatized vector of words
 #' @export
 #'
 #' @examples clema("carabinera")
-clema <- function(words) {
+clema <- function(words, replace=T) {
+
+  stopifnot(!is.na(words))
 
   value <- spacy_lemma_dictionary_es[match(words, spacy_lemma_dictionary_es$ind), ]$values
+
+  if(replace==T) {
+    value <- replace(value, is.na(value), words[is.na(value)])
+  }
+
   return(value)
 
 }
+
