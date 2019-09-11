edd51c1e1f30522722ac0059db28406838b38a7a
Roberto Salas <wala.rsalas@gmail.com>
2019-09-11 16:05:14 -0300

    Initial commit

---
+++ R/clema.R
@@ -0,0 +1,12 @@
+#' Lemmatize words using a chilean spanish dictionary
+#'
+#' @param words A word or a vector of words to lemmatize
+#'
+#' @return A converted vector of words
+#' @export
+#'
+#' @examples clema("carabinera")
+clema <- function(words) {
+  value <- lemma_dictionary_es_cl[match(words, lemma_dictionary_es_cl$ind), ]$values
+  return(value)
+}
